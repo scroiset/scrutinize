619a03c590d1d0816f92ed969675f746bf51a80c
Swann Croiset <swann.croiset@bull.net>
2014-04-23 10:05:59 +0200

    fixe syntax error in extractor

--- scrutinize/label_extractors.py
+++ scrutinize/label_extractors.py
@@ -8,7 +8,7 @@ class ArgExtract(object):
     def __init__(self, configuration):
         self.configuration = configuration
         self.arg = int(self.configuration.get('position', 0))
-        self.prefix = self.configuration.get('prefix', "label_"))
+        self.prefix = self.configuration.get('prefix', "label_")
         LOG.debug("ArgExtract position %d" % self.arg)
 
     def extract(self, *args, **kwargs):
